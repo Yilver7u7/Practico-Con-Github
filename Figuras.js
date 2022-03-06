65795ad4bb54922f22594635fccc72d03ed02ca3
yilver7u7 <yilversgg@gmail.com>
2022-03-05 22:31:40 -0500

    Version 2 loca, ya hicimos los enlaces correspondientes y la verificación de su funcionamiento correspondiente.

---
+++ Figuras.js
@@ -0,0 +1,6 @@
+//Lo primero que haremos sera definir las formulas
+//Segundo paso: Implementar las formulas en Js
+//Crear las fucciones que hagan las operaciones deseadas
+//Integrar Js con el HTML (Acá empieza lo chido mi rey)
+
+
