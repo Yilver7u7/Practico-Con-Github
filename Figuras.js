c21c9e189a6c07daa9200508b0b416dd53c33fdc
yilver7u7 <yilversgg@gmail.com>
2022-03-14 14:27:19 -0500

    De momento se a generado el formulario, sin embargo aun no se genera su correcto funcionamiento.

--- Figuras.js
+++ Figuras.js
@@ -7,11 +7,10 @@ console.log("Hola soy tu calculadora personal y estaré para ayudarte en las ope
 console.group("Cuadrado");
 //Este es el código del cuadrado
 function perimetroCuadrado(lado){
-    console.log(`Los lados del cuadrado son: ${lado}`);
-    return `${lado*4}cm`
+    return lado*4
 };
 function areaCuadrado(lado){
-    return `${lado*lado}cm²`
+    return lado*lado
 };
 console.groupEnd("Cuadrado"); 
 
@@ -19,15 +18,15 @@ console.group("Triángulos");
 //Este es el código del Triangulo
 function perimetroTriangulo(lado1,lado2,base){
     console.log(`Los lados del triángulo son ${lado1},${lado2},${base}`);
-    return `El perimetro del triángulo es: ${lado1+lado2+base}cm`
+    return lado1+lado2+base
 };
 function areaTriangulo(base,altura){
-    return `El área del triángulo es: ${(base*altura)/2}cm²`
+    return (base*altura)/2
 };
 console.groupEnd("Triángulos");
 
 console.group("Circulo");
-let PI = Math.PI;
+const PI = Math.PI;
 // const perimetroCirculo = diametroCirculo*PI;
 // const areaCirculo = (radioCirculo*radioCirculo)*PI;
 function diametroCirculo(radio){
@@ -39,7 +38,25 @@ function perimetroCirculo(radio){
 };
 
 function areaCirculo(radio){
-    return `${(radio*radio)*PI}cm²`
+    return (radio*radio)*PI
 };
 
 console.groupEnd("Circulo");
+
+//Operaciones de los botones (Cuadrado)
+//Perimetro Cuadrado
+function calcularPerimetroCuadrado(lado){
+    //Hacemos el uso del lector documen.get para traernos del HTMl a nuestro formulario y darle ese valor a una variable
+    const input = document.getelementById("InputCuadrado");
+    //Luego dentro de está buscamos su proiedad o valor y se lo declaramos a una nueva variable que sera usada, para las operaciones.
+    const value = input.value;
+    //generamos una variableque reutilizado los valores anteriores les da el argumento que el usuario les entrego
+    const perimetro= perimetroCuadrado(value);
+    alert(perimetro);
+};
+function calcularAreaCuadrado(){ 
+    const input = document.getElementById("InputCuadrado");
+    const value = input.value;
+    const area = areaCuadrado(value);
+    alert(area); 
+};
