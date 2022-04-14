413e649f58bb51d576b5bfd9c8cc1b4ddf4b00f0
yilver7u7 <yilversgg@gmail.com>
2022-04-14 12:02:07 -0500

    Termine la parte del JS, me falta por realizar las areas de HTML Y CCS, para crear una página web mucho más dinámica y llamativa, así que me pondré a estudiar seguramente Bootstrap y volverme muy bueno en CSS, para tener un excelente portafolio con este proyecto que e estado llevando acabo en mi aprendizaje.

--- Javascript/Salarios/Analisis.js
+++ Javascript/Salarios/Analisis.js
@@ -1,3 +1,15 @@
+//Helpers
+//Creamos la función que se encargara de generar nuestro promedio cuando la lista de personas es par 
+function calcularMedia (a,b){
+    const suma = a+b;
+    return suma/2;
+};
+
+function esPar (numerito){
+    return (numerito%2 === 0);
+};
+//Calculamos la mediana General
+
 //Llamamos nuestro variable de personas, usamos el metodo map para filtrar y crear un nuevo Array que contenga el salario de cada uno de ellos
 const salariosCol = colombia.map(
     personita => {
@@ -6,5 +18,39 @@ const salariosCol = colombia.map(
 );
 //Una vez hecho esto, ordenaremos de menor a mayor, para que de está forma podamos sacar la mediana con facilidad
 //Usamos una Arrow Function de una operación en una linea para agilizar el proceso y ahorrar el consumo de recursos
-const salariosColSorted = salariosCol.sort((a,b)=>{return a-b});
+const salariosColSorted = salariosCol.sort((a,b)=>{return a-b;});
+
+function medianaSalarios(lista){
+    const mitad = parseInt(lista.length/2);
+    if (esPar(lista.length)) {
+        //Buscamos los elementos que esten en la mitad paa promediarlos
+        const personitaMitad1 = lista[mitad-1];
+        const personitaMitad2 = lista[mitad];
+        //Hacemos uso de la función calcularMedia para hallar nuestra mediana
+        const mediana = calcularMedia(personitaMitad1, personitaMitad2);
+        return mediana;
+    }else{
+        //Hallamos la mitad de nuestra lista para saber nuestro mediana
+        const personitaMitad = lista[mitad];
+        return personitaMitad;
+    }
+};
+
+const medianaGeneralCol = medianaSalarios(salariosColSorted);
+
+
+//Mediana top 10% 
+//Uso del metodo Splice, el cuál nos permite cortar la parte deseada de nuestro Array para utilizarla posteriormente
+const spliceStar = (salariosColSorted.length * 90)/100;
+const spliceCount = salariosColSorted.length -spliceStar;
+const salariosColTop10 = salariosColSorted.splice(spliceStar, spliceCount);
+
+//Traemos el resultado de nuestro metodo
+const medianaTop10Col = medianaSalarios(salariosColTop10);
+
+
+console.log({
+    medianaGeneralCol,
+    medianaTop10Col,
+});
 
